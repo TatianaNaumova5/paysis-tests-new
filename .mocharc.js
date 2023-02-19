b6e02de4d26d429395cc1e8c2cf8aece104546dd
Tatiana Naumova <tatiananaumova5@gmail.com>
2023-02-18 19:24:43 -0500

    auth tests

--- .mocharc.js
+++ .mocharc.js
@@ -1,4 +1,6 @@
-module.exports = {
+ module.exports = {
   require: '@babel/register',
   spec: 'specs/*.spec.js',
+  ignore: 'specs/example.spec.js',
+  timeout: 15000,
 }
--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -1,7 +1,6 @@
 import {expect} from 'chai';
 import request from 'supertest';
-describe('Authentication', () => {
-
+describe('Authentication positive', () => {
   it('Successful log in', async ()=> {
     const response = await request('http://localhost:3000')
       .post('/auth')
@@ -9,4 +8,40 @@ describe('Authentication', () => {
     await expect(response.statusCode).to.eq(200)
   })
 
+    it('Successful log in', async ()=> {
+      const response = await request('http://localhost:3000')
+        .post('/auth')
+        .send({login: 'admin', password: 'admin'})
+      await expect(response.body.token).not.to.be.null
+    })
+
+  it('Successful log in', async ()=> {
+    const response = await request('http://localhost:3000')
+      .post('/auth')
+      .send({login: 'admin', password: 'admin'})
+    await expect(response.body.token).to.be.a('string')
+  })
+})
+
+describe('Authentication negative', () => {
+  it('Log in with wrong login', async ()=> {
+    const response = await request('http://localhost:3000')
+      .post('/auth')
+      .send({login: 'admin888', password: 'admin'})
+    await expect(response.statusCode).to.eq(404)
+  })
+
+  it('Log in with wrong login', async ()=> {
+    const response = await request('http://localhost:3000')
+      .post('/auth')
+      .send({login: 'admin888', password: 'admin'})
+    await expect(response.body.token).to.be.null
+  })
+
+  it('Log in with wrong login', async ()=> {
+    const response = await request('http://localhost:3000')
+      .post('/auth')
+      .send({login: 'admin888', password: 'admin'})
+    await expect(response.body.message).to.eq('Wrong login or password.')
+  })
 })
