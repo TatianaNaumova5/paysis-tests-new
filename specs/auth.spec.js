de8824445184bda9bfa4b83d4089e1f51f192170
Tatiana Naumova <tatiananaumova5@gmail.com>
2023-02-18 19:38:10 -0500

    added auth tests

--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -32,12 +32,6 @@ describe('Authentication negative', () => {
     await expect(response.statusCode).to.eq(404)
   })
 
-  it('Log in with wrong login', async ()=> {
-    const response = await request('http://localhost:3000')
-      .post('/auth')
-      .send({login: 'admin888', password: 'admin'})
-    await expect(response.body.token).to.be.null
-  })
 
   it('Log in with wrong login', async ()=> {
     const response = await request('http://localhost:3000')
