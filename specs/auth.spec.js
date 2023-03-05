848b3b6626533b74e0bb0f770e4ec02bcd5d9e71
Tatiana Naumova <tatiananaumova5@gmail.com>
2023-03-04 20:35:40 -0500

    added scripts for prettier and lint

--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -1,24 +1,26 @@
 import {expect} from 'chai';
 import request from 'supertest';
+import 'dotenv/config'
+
 describe('Authentication positive', () => {
   it('Successful log in', async ()=> {
-    const response = await request('http://localhost:3000')
+    const response = await request(process.env.BASE_URL)
       .post('/auth')
-      .send({login: 'admin', password: 'admin'})
+      .send({login: process.env.LOGIN, password: process.env.PASSWORD})
     await expect(response.statusCode).to.eq(200)
   })
 
     it('Successful log in', async ()=> {
-      const response = await request('http://localhost:3000')
+      const response = await request(process.env.BASE_URL)
         .post('/auth')
-        .send({login: 'admin', password: 'admin'})
+        .send({login: process.env.LOGIN, password: process.env.PASSWORD})
       await expect(response.body.token).not.to.be.null
     })
 
   it('Successful log in token is a string', async ()=> {
-    const response = await request('http://localhost:3000')
+    const response = await request(process.env.BASE_URL)
       .post('/auth')
-      .send({login: 'admin', password: 'admin'})
+      .send({login: process.env.LOGIN, password: process.env.PASSWORD})
     await expect(response.body.token).to.be.a('string')
   })
 })
@@ -26,30 +28,30 @@ describe('Authentication positive', () => {
 
 describe('Authentication negative', () => {
   it('Log in with wrong login', async ()=> {
-    const response = await request('http://localhost:3000')
+    const response = await request(process.env.BASE_URL)
       .post('/auth')
-      .send({login: 'admin888', password: 'admin'})
+      .send({login: 'admin888', password:process.env.PASSWORD})
     await expect(response.statusCode).to.eq(404)
   })
 
   it('Log in with wrong login', async ()=> {
-    const response = await request('http://localhost:3000')
+    const response = await request(process.env.BASE_URL)
       .post('/auth')
-      .send({login: 'admin888', password: 'admin'})
+      .send({login: 'admin888', password:process.env.PASSWORD})
     await expect(response.body.message).to.eq('Wrong login or password.')
   })
 
   it('Log in with wrong login', async ()=> {
-    const response = await request('http://localhost:3000')
+    const response = await request(process.env.BASE_URL)
       .post('/auth')
-      .send({login: 'admin888', password: 'admin'})
+      .send({login: 'admin888', password:process.env.PASSWORD})
     await expect(response.body.token).to.be.undefined
   })
 
   it('Uncceassful log in with wrong end point', async ()=> {
-    const response = await request('http://localhost:3000')
+    const response = await request(process.env.BASE_URL)
       .post('/auth111')
-      .send({login: 'admin', password: 'admin'})
+      .send({login: process.env.LOGIN, password: process.env.PASSWORD})
     await expect(response.statusCode).to.eq(404)
   })
 })
