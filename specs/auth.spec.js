5877011448fc0111a675a55a61a31616e0d4b8bc
Tatiana Naumova <tatiananaumova5@gmail.com>
2023-03-09 17:03:47 -0500

    commit auth.spec

--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -25,41 +25,64 @@ describe('Authentication positive', () => {
       expect(response.body.token).not.to.be.a('number')
     })
   })
-})
 
-describe.skip('Authentication negative', () => {
-  it('Log in with wrong login', async () => {
-    const response = await request(process.env.BASE_URL)
-      .post('/auth')
-      .send({login: 'test123', password: process.env.PASSWORD})
-    await expect(response.statusCode).to.eq(404)
+  describe('Log in with wrong login', ()=>{
+    let response
+
+    before(async  () =>{
+      response = await logIn('invalid', process.env.PASSWORD)
+    })
+
+    it('Response status code is 404',  () => {
+      expect(response.statusCode).to.eq(404)
+    })
+
+    it('Response body returnes an arror message',  () => {
+      expect(response.body.message).to.eq('Wrong login or password.')
+    })
+
+    it('Response body returnes an arror message',  () => {
+      expect(response.body.message).to.be.a('string')
+    })
   })
 
-  it('Log in with wrong password', async () => {
-    const response = await request(process.env.BASE_URL)
-      .post('/auth')
-      .send({login: process.env.LOGIN, password: 'test123'})
-    await expect(response.statusCode).to.eq(404)
+  describe('Log in with wrong password', ()=>{
+    let response
+
+    before(async  () =>{
+      response = await logIn(process.env.LOGIN, 'invalid')
+    })
+
+    it('Response status code is 404',  () => {
+      expect(response.statusCode).to.eq(404)
+    })
+
+    it('Response body returnes an arror message',  () => {
+      expect(response.body.message).to.eq('Wrong login or password.')
+    })
+
+    it('Response body returnes an arror message',  () => {
+      expect(response.body.message).to.be.a('string')
+    })
   })
 
-  it('Log in with wrong login', async () => {
-    const response = await request(process.env.BASE_URL)
-      .post('/auth')
-      .send({login: 'test123', password: process.env.PASSWORD})
-    await expect(response.body.message).to.eq('Wrong login or password.')
-  })
+  describe('Log in without credentials', ()=> {
+    let response
 
-  it('Log in with wrong login', async () => {
-    const response = await request(process.env.BASE_URL)
-      .post('/auth')
-      .send({login: 'test123', password: process.env.PASSWORD})
-    await expect(response.body.token).to.be.undefined
-  })
+    before(async () => {
+      response = await logIn()
+    })
 
-  it('Uncceassful log in with wrong end point', async () => {
-    const response = await request(process.env.BASE_URL)
-      .post('/auth111')
-      .send({login: process.env.LOGIN, password: process.env.PASSWORD})
-    await expect(response.statusCode).to.eq(404)
+    it('Response status code is 400', () => {
+      expect(response.statusCode).to.eq(400)
+    })
+
+    it('Response body returnes an arror message', () => {
+      expect(response.body.message).to.eq('No credentials provided.')
+    })
+
+    it('Response body returnes an arror message', () => {
+      expect(response.body.message).to.be.a('string')
+    })
   })
-})
+  })
