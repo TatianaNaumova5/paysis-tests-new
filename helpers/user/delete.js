6347888d432ee3e6668c48fa3cf3cfbb9d4fc870
Tatiana Naumova <tatiananaumova5@gmail.com>
2023-03-09 17:52:19 -0500

    added tests in user.spec

--- helpers/user/delete.js
+++ helpers/user/delete.js
@@ -1,5 +1,6 @@
 import {client} from '../client';
 
-export async function delete_(id){
+async function delete_(id){
   return client.delete('/users').set('Authorization', `Bearer ${process.env.TOKEN}`).send({id})
 }
+export {delete_ as delete}
